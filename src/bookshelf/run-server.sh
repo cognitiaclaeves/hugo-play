df05d5122991d7c25d41b53ce8b47177f7ac397f
J . Norment <cognitiaclaeves@gmail.com>
2017-04-30 16:15:14 +0000

    layout stuff

--- src/bookshelf/run-server.sh
+++ src/bookshelf/run-server.sh
@@ -1,3 +1,5 @@
 
-hugo server --baseURL=http://pwd10_0_3_3-1313.host1.labs.play-with-docker.com --port=1313 --appendPort=true --bind=0.0.0.0 --buildDrafts --theme=hugo_theme_robust
+pwd_link='http://pwd10_0_49_3-1313.host1.labs.play-with-docker.com/'
+
+hugo server --baseURL="$pwd_link" --port=1313 --appendPort=true --bind=0.0.0.0 --buildDrafts --theme=hugo_theme_robust --watch
 
